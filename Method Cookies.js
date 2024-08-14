2f2b021cf6ab69b388475a6cbe6a0a5c5344a490
D.B Nhàn <117060625+Mohick@users.noreply.github.com>
2024-08-14 16:57:20 +0700

    Add files via upload

---
+++ Method Cookies.js
@@ -0,0 +1,15 @@
+const { setDateCookies } = require("../JWT/JWT");
+const env = require("../Object ENV/Object ENV");
+
+
+
+const methodsCookie = {
+    expires: setDateCookies,
+    secure: `${env.EXPRESS_COOKIES_SECURE}`.trim().toLowerCase() == "true".trim().toLowerCase(),
+    httpOnly: `${env.EXPRESS_COOKIES_ONLYHTTP}`.trim().toLowerCase() == "true".trim().toLowerCase(),
+    sameSite: "None"
+}
+
+
+
+module.exports = methodsCookie

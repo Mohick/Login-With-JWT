79ab36e11ce87d9888377680271739312b15a647
D.B Nhàn <117060625+Mohick@users.noreply.github.com>
2024-08-25 17:24:41 +0700

    Add files via upload
    update logout

--- src/MVC/Model/Logout/Logout.js
+++ src/MVC/Model/Logout/Logout.js
@@ -1,8 +1,9 @@
+const methodsCookie = require("../../../Config/Cookies/Method Cookies");
 
 
 class LogoutAccount {
     async logout(req, res) {
-        res.clearCookie('authToken');
+        res.clearCookie('authToken',methodsCookie);
         res.json({
             valid: true,
             message: 'Logged out successfully'
